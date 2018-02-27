e0a5d69b93ccd758efb3e60fc6393604a4ea3cd2
raretp <raretp@ttu.ee>
2018-02-27 11:30:36 +0200

    Aurelia v.0.3.

--- client/src/homework/homework.js
+++ client/src/homework/homework.js
@@ -1,3 +1,43 @@
-export class Homework {
-	
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+export class CreatNewSubject {
+
+	getSubjectDetails() { 
+		let client = new HttpClient();
+		let homeWorkTableData = new Object();
+
+	    client.fetch('http://localhost:8080/subject/get/mari', {
+	    	'method': "POST",
+	    	'body': json(this.userData)
+	    })
+	        .then(response => response.json())
+	        .then(data => {
+				homeWorkTableData.aine = data[0].nimi;
+				homeWorkTableData.opnimi = data[0].oppejoud;
+				homeWorkTableData.ainekood = data[0].ainekood;
+				this.getHomeworks("1", homeWorkTableData);
+	        	console.log("Server saatis: " + JSON.stringify(data));
+	    });
+			console.log("getSubjectDetails method executed!");
+	}
+
+	getHomeworks(ID, tableData) {
+		let client = new HttpClient();
+		let parsedData = "";
+	    client.fetch('http://localhost:8080/task/get/' + ID, {
+	    	'method': "POST",
+	    	'body': json(this.userData)
+	    })
+	        .then(response => response.json())
+	        .then(data => {
+				console.log("Server saatis: " + JSON.stringify(data));
+				let yl = [];
+				for(var i = 0; i < data.length; i++) {
+					yl.push(data[i].ylesanne_tekst);
+				}
+	        	tableData.yl = yl;
+	        	console.log(tableData);
+	    });
+			console.log("getHomeworks method executed!");
+	}
 }
