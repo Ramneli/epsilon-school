93a8112039ac73d4f195f19d3a3f36b032188ad6
Jordan <jordanjors@gmail.com>
2018-03-05 20:16:11 +0200

    Improvements in layout and design on all pages.

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -1,4 +1,24 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class Home {
-	constructor() {
+	userData = {};
+
+	addSubject() {
+		let client = new HttpClient();
+		let url = 'http://localhost:8080/subject/add';
+
+		console.log("Serverile saadetakse: " + JSON.stringify(this.userData));
+	    client.fetch(url, {
+	    	'method': "POST",
+	    	'body': json(this.userData)
+	    })
+	        .then(response => response.json())
+	        .then(data => {
+				
+	        	console.log("Server saatis: " + data);
+	    });
+			console.log("addSubject method executed!");
 	}
 }
+
+
