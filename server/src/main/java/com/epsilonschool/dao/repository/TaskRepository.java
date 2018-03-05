26afe49575b25db5b042e94b53250e65fa88966a
Jordan <jordanjors@gmail.com>
2018-03-05 20:33:16 +0200

    Added deadline check for tasks.

--- server/src/main/java/com/epsilonschool/dao/repository/TaskRepository.java
+++ server/src/main/java/com/epsilonschool/dao/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package com.epsilonschool.dao.repository;
 
 import com.epsilonschool.entity.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ public interface TaskRepository extends CrudRepository<Task, String> {
     // TODO: Return all tasks that are relevant: task date > current date. Use @Query.
 
 
+    @Query(value = "SELECT * FROM task WHERE deadline >= CURDATE()", nativeQuery = true)
     List<Task> findAll();
 }
