6127c38bdba40c2a17352484c9b650490a7a7cdd
Jordan <jordanjors@gmail.com>
2018-04-24 21:16:09 +0300

    Added bootstrap alert when saving settings

--- angular-client/src/app/settings/settings.component.ts
+++ angular-client/src/app/settings/settings.component.ts
@@ -9,6 +9,25 @@ export class SettingsComponent implements OnInit {
 
   constructor() { }
 
+
+  displaySuccessAlert() {
+    var successAlert = document.createElement("div");
+		successAlert.setAttribute('id', "successAlert");
+		successAlert.setAttribute("class", "alert alert-success");
+		successAlert.appendChild(document.createTextNode('Seaded on salvestatud!'));
+
+		var alertDiv = document.getElementById('alert');
+		alertDiv.appendChild(successAlert);
+		this.startAlertTimeout();
+	}
+
+	startAlertTimeout() {
+	    setTimeout(function () {
+	    	var successAlert = document.getElementById('successAlert');
+	    	successAlert.parentNode.removeChild(successAlert);
+	    }, 3000);
+	}
+
   ngOnInit() {
   }
 
