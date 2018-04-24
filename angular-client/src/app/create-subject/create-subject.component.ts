9e7b0d6e93a4aa35ce0b2f4e999959719932ba39
raretp <raretp@ttu.ee>
2018-04-24 20:14:51 +0300

    Added form validation, success message for said form validationand timeout function for one popup.

--- angular-client/src/app/create-subject/create-subject.component.ts
+++ angular-client/src/app/create-subject/create-subject.component.ts
@@ -12,7 +12,7 @@ export class CreateSubjectComponent implements OnInit {
 
   	constructor(private subjectService: SubjectService, private authService : AuthService) { }
 
-	addSubjectToTimetable(userSubjectName, userSubjectCode, userLecturerName, userChoice) {
+	addSubjectToTimetable(userSubjectName, userSubjectCode, userLecturerName, userChoice, createSubjectForm) {
 		let userData = {
 			name: userSubjectName,
 			code: userSubjectCode,
@@ -21,18 +21,36 @@ export class CreateSubjectComponent implements OnInit {
 		};
 
 		console.log(userSubjectName, userSubjectCode, userLecturerName, userChoice);
-
-		if (userSubjectName.length == 0) {
-			alert('Please check your inputs.');
-		} else if (userSubjectCode.length == 0) {
-			alert('Please check your inputs.');
-		} else if (userLecturerName.length == 0) {
-			alert('Please check your inputs.');
+		
+		
+		if (userSubjectName.length == 0 || userSubjectCode.length == 0 || userLecturerName.length == 0) {
+			var objectsToValidate = document.getElementsByClassName("needs-validation");
+			objectsToValidate[0].className = "was-validated";
 		} else {
 			this.subjectService.createNewSubject(userData).subscribe();
-			alert("Aine loodud.");
+			this.displayMessage();
+			var x : HTMLFormElement;
+			x = <HTMLFormElement>document.getElementById("createSubjectForm");
+			x.reset();
 		}
 	}
+
+	displayMessage() {
+		var successMsgDiv = document.createElement('div');
+		successMsgDiv.setAttribute('id', "successMsgDiv");
+		successMsgDiv.setAttribute('style', 'height: 40px; background-color: #39ef54; border-radius: 3px;');
+		successMsgDiv.appendChild(document.createTextNode('Success'));
+		var div2 = document.getElementById('validation-form');
+		div2.appendChild(successMsgDiv);
+		this.timeout();
+	}
+
+	timeout() {
+	    setTimeout(function () {
+	    	var successMsgDiv = document.getElementById('successMsgDiv');
+	    	successMsgDiv.parentNode.removeChild(successMsgDiv);
+	    }, 3000);
+	  } 
 
 	ngOnInit() {
     	console.log(this.authService.getUserId());
